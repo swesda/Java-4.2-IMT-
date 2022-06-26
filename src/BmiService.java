1aa717f98a58f6fc133a2a4b33e3431b5e3a1b7b
swesda <105632993+swesda@users.noreply.github.com>
2022-06-26 21:55:50 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
-
     public double calculate(double weight, double height) {
         int result = (int) (weight / (height * height));
-
         return result;
     }
 }
